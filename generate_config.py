11d970329f5d0f6a6427c8ab635ef680fdfa36b3
T. Yamamoto <tyamamoto-st@keio.jp>
2021-04-16 22:38:10 +0900

    modifies generate_config.py

--- generate_config.py
+++ generate_config.py
@@ -49,7 +49,7 @@ def save_file(filename, atoms):
 
 atoms = []
 
-add_ball(atoms, -20, 2.0)
-add_ball(atoms, 20, -2.0)
+add_ball(atoms, -20, 10.0)
+add_ball(atoms, 20, -10.0)
 
 save_file("collision.atoms", atoms)
